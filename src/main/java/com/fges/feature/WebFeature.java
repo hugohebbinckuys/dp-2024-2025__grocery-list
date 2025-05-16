e721d3380fee6463447140da6b59fce93de1431d
hugohebbinckuys <hugo29.heb@gmail.com>
2025-05-16 13:22:59 +0200

    fix port issue

--- src/main/java/com/fges/feature/WebFeature.java
+++ src/main/java/com/fges/feature/WebFeature.java
@@ -8,17 +8,18 @@ import fr.anthonyquere.GroceryShopServer;
 import java.util.ArrayList;
 
 public class WebFeature implements Feature {
-    private int port;
+    private String[] port;
 
-    public WebFeature(int port) {
-        this.port = port;
+    public WebFeature(String[] port) {
+        this.port = new String[1];
+        this.port[0] = port[0];
     }
 
     @Override
     public int execute(ArrayList<ProductItem> listItems, ProductItem item) {
         com.fges.web.MainWeb.main(this.port);
         GroceryShopServer server = new GroceryShopServer(new SimpleGroceryShop());
-        server.start(this.port);
+//        server.start(this.port);
         return 0;
     }
 }
--- src/main/java/com/fges/web/MainWeb.java
+++ src/main/java/com/fges/web/MainWeb.java
@@ -4,9 +4,11 @@ import fr.anthonyquere.GroceryShopServer;
 import fr.anthonyquere.MyGroceryShop;
 
 public class MainWeb {
-    public static void main(int port) {
+    public static void main(String [] args) {
         MyGroceryShop groceryShop = new SimpleGroceryShop();
         GroceryShopServer server = new GroceryShopServer(groceryShop);
+        int port = Integer.parseInt(args[0]);
+
         server.start(port);
 
         System.out.println("Grocery shop server started at http://localhost:" + port);
